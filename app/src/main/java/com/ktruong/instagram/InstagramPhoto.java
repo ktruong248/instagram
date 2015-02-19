e5819a0b931af97e468cc81665992bbeaf4c4ce1
khiem <ktruong248@yahoo.com>
2015-02-19 08:48:31 -0800

    initial checkin for instagam with display profile icon and image

---
+++ app/src/main/java/com/ktruong/instagram/InstagramPhoto.java
@@ -0,0 +1,61 @@
+package com.ktruong.instagram;
+
+
+import android.net.Uri;
+
+public class InstagramPhoto {
+    private String name;
+    private String userFullName;
+    private long id;
+    private Uri profileImageUrl;
+    private int commentCount;
+    private Uri imageUrl;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserFullName() {
+        return userFullName;
+    }
+
+    public void setUserFullName(String userFullName) {
+        this.userFullName = userFullName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Uri getProfileImageUrl() {
+        return profileImageUrl;
+    }
+
+    public void setProfileImageUrl(Uri profileImageUrl) {
+        this.profileImageUrl = profileImageUrl;
+    }
+
+    public int getCommentCount() {
+        return commentCount;
+    }
+
+    public void setCommentCount(int commentCount) {
+        this.commentCount = commentCount;
+    }
+
+    public void setImageUrl(Uri imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Uri getImageUrl() {
+        return imageUrl;
+    }
+}
---
+++ app/src/main/java/com/ktruong/instagram/InstagramPhotoAdapter.java
@@ -0,0 +1,62 @@
+package com.ktruong.instagram;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Movie;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.makeramen.RoundedTransformationBuilder;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Transformation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ktruong on 2/18/15.
+ */
+public class InstagramPhotoAdapter extends ArrayAdapter<InstagramPhoto> {
+
+    public InstagramPhotoAdapter(Context context, List<InstagramPhoto> photos) {
+        super(context,0, photos);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_photo, parent, false);
+        }
+
+        InstagramPhoto item = getItem(position);
+        ImageView profileImage = (ImageView)convertView.findViewById(R.id.profileImage);
+        profileImage.setImageResource(0); //clear
+
+        Transformation transformation = new RoundedTransformationBuilder()
+                .borderColor(Color.TRANSPARENT).scaleType(ImageView.ScaleType.FIT_CENTER)
+                .borderWidthDp(3)
+//                .cornerRadius(500)
+                .cornerRadiusDp(30)
+//                .oval(true)
+                .build();
+        Picasso.with(getContext()).load(item.getProfileImageUrl()).transform(transformation).resize(150,0).into(profileImage);
+
+        ImageView photoImageView = (ImageView) convertView.findViewById(R.id.photoImage);
+        photoImageView.setImageResource(0);
+        Picasso.with(getContext()).load(item.getImageUrl()).into(photoImageView);
+
+//        TextView commentText = (TextView)convertView.findViewById(R.id.commentText);
+//        commentText.setText(item.getComment());
+
+        // findByviewId also slow so there are technique to optimized
+//        tvTitle.setText(item.getTitle());
+//        tvScore.setText(item.creditScoreLabel());
+
+        return convertView;
+    }
+}
